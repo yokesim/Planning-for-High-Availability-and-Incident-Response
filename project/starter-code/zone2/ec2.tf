7d4ea642e4ec7d76a8e7e1bf677f592e52ac71d9
yokesim <32306418+yokesim@users.noreply.github.com>
2025-12-11 17:50:05 +0800

    update zone2 ami

--- project/starter-code/zone2/ec2.tf
+++ project/starter-code/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0f989eeb7dfb0210e"
+   aws_ami            = "ami-00a21f851c5b62c83"
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
